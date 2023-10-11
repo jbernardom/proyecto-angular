4139e4f894c12e841beca09cdd1a93496bf51146
Bernardo Martinez <jbernardom1@hotmail.com>
2023-10-10 20:22:54 -0500

    finalizado

--- src/BACK/routes/capturar_datos_api.php
+++ src/BACK/routes/capturar_datos_api.php
@@ -1,18 +1,24 @@
 <?php
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization, Accept");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PATCH,TRACE");
 header("Access-Control-Allow-Origin: http://localhost:4200");
-header("Access-Control-Expose-Headers: *");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization, Accept");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Expose-Header: *");
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+
 $json = file_get_contents('php://input');
-return json_decode($json,true);
+$data = json_decode($json, true);
 
 if ($data) {
-    echo json_encode(array('message' => 'Datos recibidos y procesados correctamente.'));
 
+    echo json_encode(array('message' => 'Datos recibidos y procesados correctamente.'));
 } else {
     http_response_code(400);
     echo json_encode(array('error' => 'Error al procesar los datos.'));
 }
-
-
